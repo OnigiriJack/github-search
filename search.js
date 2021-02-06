35a4d2cda41e2b819047c83ee4a050d931d9acd9
OnigiriJack <jackmihalik@icloud.com>
2021-02-06 18:33:24 +0900

    fixed setTimeout bug, incremental search sometimes odd

--- search.js
+++ search.js
@@ -1,34 +1,35 @@
 window.onload = function () {
     let apiCallCount = 0;
-    let throttled = false;
-    // 1 call/Minute 
-
-    //if more then ten calls a minute then throttle for 1 minute
-    //reset throttle
-    window.setInterval(() => {
-        throttled = false
-        apiCallCount = 0;
-    }, 10000);
+    let throttled = true;
     let input = document.querySelector("input");
+    let previousInput = input.value;
     let repoList = document.createElement("ul");
     repoList.className = "repos"
     let app = document.getElementById("app")
     app.append(repoList)
 
-    input.oninput = () => {
+    // every ten seconds reset the throttle
+    window.setInterval(() => {
+        throttled = false;
+        document.getElementById("warning").style.display = "none";
+        apiCallCount = 0;
+    }, 15000);
 
-        setTimeout(function () {
-            if (throttled === false) {
-                //getRepoInfo();
-                apiCallCount++;
-            } else {
-                console.log("request limit exceeded")
-            }
-            if (apiCallCount > 3) throttled = true;
-            console.log("API COUNT: " + apiCallCount)
-            console.log("throttled: " + throttled)
-        }, 1000)
-    }
+    window.setInterval(() => {
+
+        if (input.value != previousInput && throttled === false) {
+            console.log("called" + "previous " + previousInput + " current " + input.value)
+            getRepoInfo();
+            apiCallCount++;
+        }
+        if (apiCallCount > 2) {
+            throttled = true;
+            document.getElementById("warning").style.display = "block";
+        }
+        console.log(apiCallCount)
+        console.log("previous " + previousInput + " current " + input.value)
+        previousInput = input.value;
+    }, 2000)
 
     getRepoInfo = () => {
         fetch(`https://api.github.com/search/repositories?q=${input.value}`,
@@ -39,6 +40,7 @@ window.onload = function () {
             })
             .then(response => response.json())
             .then(data => {
+                //console.log(data)
                 data.items.map(repo => {
                     let repoObj = {
                         url: repo.svn_url,
