b33173d367ce891d92fdd56b875db814390eb3c2
OnigiriJack <jackmihalik@icloud.com>
2021-02-06 13:48:38 +0900

    Live input from user vanilla js

--- search.js
+++ search.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+window.onload = function () {
+    console.log("hello world")
+
+    let input = document.querySelector("input");
+    let output = document.createElement("p");
+    output.className = "output"
+    output.textContent = "jack"
+    let app = document.getElementById("app")
+    console.log("app" + app)
+    app.append(output)
+
+    input.oninput = () => {
+        output.textContent = input.value;
+    }
+}
+
+
