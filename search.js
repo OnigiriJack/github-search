d6dc0781a0c39247633411a3c6cb740d48156e5a
OnigiriJack <jackmihalik@icloud.com>
2021-02-06 16:13:42 +0900

    Able to append a list of repos with hrefs on input change

--- search.js
+++ search.js
@@ -2,23 +2,47 @@ window.onload = function () {
     console.log("hello world")
 
     let input = document.querySelector("input");
-    let output = document.createElement("p");
+    let output = document.createElement("ul");
     output.className = "output"
-    output.textContent = "jack"
+
     let app = document.getElementById("app")
     console.log("app" + app)
     app.append(output)
 
     input.oninput = () => {
-        output.textContent = input.value;
+
+        setTimeout(GetRepoistoryInfo, 500)
     }
 
     GetRepoistoryInfo = () => {
-        fetch("https://api.github.com/search/repositories", { q: "q" })
+        fetch(`https://api.github.com/search/repositories?q=${input.value}`,
+            {
+                headers: {
+                    'Accept': 'application/vnd.github.v3+jsonn',
+                },
+            })
             .then(response => response.json())
-            .then(data => console.log(data))
+            .then(data => {
+                console.log(data)
+                let repos = data.items;
+
+                repos.map(repo => {
+                    let repoObj = {
+                        url: repo.svn_url,
+                        name: repo.name,
+                    }
+                    let repoEl = document.createElement("li");
+                    let a = document.createElement("a");
+                    a.textContent = `${repoObj.name}`;
+                    a.setAttribute("href", repoObj.url)
+                    repoEl.appendChild(a)
+                    output.appendChild(repoEl);
+                })
+
+
+            })
     }
-    GetRepoistoryInfo()
+
 }
 
 
